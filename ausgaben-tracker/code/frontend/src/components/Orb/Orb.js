50ca0c6bd1d47814eb34933e10997ee72997d3c9
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-20 09:43:18 +0200

    Orb WindowSize

--- ausgaben-tracker/code/frontend/src/components/Orb/Orb.js
+++ ausgaben-tracker/code/frontend/src/components/Orb/Orb.js
@@ -1,7 +1,24 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { useWindowSize } from '../../utils/useWindowSize';
 
 function Orb() {
+
+    const {width, height} = useWindowSize()
+
+    console.log(width, height)
+
+    const moveOrb = keyframes`
+        0%{
+            transform: translate(0, 0);
+        }
+        50%{
+            transform: translate(${width/1.2}px, ${height/2}px);
+        }
+        100%{
+            transform: translate(0, 0);
+        }
+    `
 
      const OrbStyled = styled.div`
         width: 70vh;
@@ -11,7 +28,8 @@ function Orb() {
         margin-left: -37vh;
         margin-top: -37vh;
         background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-        filter: blur(100px);
+        filter: blur(200px);
+        animation: ${moveOrb} 15s alternate linear infinite;
     `;
 
     return (
