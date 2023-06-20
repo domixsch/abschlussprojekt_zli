020c0293b744be04a5500f6ffb5e6619b1d3a381
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-20 15:53:07 +0200

    ActiveState added

--- ausgaben-tracker/code/frontend/src/App.js
+++ ausgaben-tracker/code/frontend/src/App.js
@@ -3,18 +3,42 @@ import styled from 'styled-components'
 import { MainLayout } from './styles/layouts';
 import Orb from './components/Orb/Orb';
 import Navigation from './components/Navigation/Navigation';
-import { useState } from 'react';
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
+import Dashboard from './components/Dashboard/Dashboard';
+import Income from './components/Income/Income'
+import Expenses from './components/Expenses/Expenses';
 
 
 function App() {
   const [active, setActive] = useState(1)
 
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>
+          {displayData}
+        </main>
       </MainLayout>
     </AppStyled>
   );
