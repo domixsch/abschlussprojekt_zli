445205847799ab4f37df869f4f2c0bcada2b455d
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-20 08:36:42 +0200

    images added

--- ausgaben-tracker/code/frontend/src/App.js
+++ ausgaben-tracker/code/frontend/src/App.js
@@ -1,10 +1,33 @@
+import bg from './img/bg.png'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <AppStyled bg={bg} className="App">
+
+    </AppStyled>
   );
 }
+  const AppStyled = styled.div`
+  <AppStyled
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+
+
+  
 
 export default App;
