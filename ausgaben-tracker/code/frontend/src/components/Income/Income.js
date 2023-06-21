2d96aee2432253a8d0187ffbe82a96470f5baa52
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-21 10:30:22 +0200

    IncomeItems

--- ausgaben-tracker/code/frontend/src/components/Income/Income.js
+++ ausgaben-tracker/code/frontend/src/components/Income/Income.js
@@ -1,11 +1,17 @@
-import React from 'react'
 import styled from 'styled-components';
 import { InnerLayout } from '../../styles/layouts';
 import { useGlobalContext } from '../../context/globalContext';
 import Form from '../Form/Form';
+import React, { useEffect } from 'react'
+
 
 function Income() {
-  const {addIncome} = useGlobalContext()
+  const {addIncome, incomes, getIncomes} = useGlobalContext()
+
+  useEffect(() =>{
+    getIncomes()
+  }, [])
+
   return (
     <IncomeStyled>
       <InnerLayout>
@@ -15,7 +21,19 @@ function Income() {
             <Form />
           </div>
           <div className='incomes'>
-
+            {incomes.map((income) => {
+              const {_id, title, amount, date, category, description} = income;
+              return <incomeItems
+              key={_id}
+              id={_id}
+              title={title}
+              description={description}
+              amount={amount}
+              date={date}
+              category={category}
+              indicatorColor="var(--color-green)"
+              />
+            })}
           </div>
         </div>
       </InnerLayout>
