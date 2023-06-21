48ec71c0861897cf07dddb2bcdc4e7a040266437
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-21 09:03:23 +0200

    Form and Button fixed

--- ausgaben-tracker/code/frontend/src/components/Income/Income.js
+++ ausgaben-tracker/code/frontend/src/components/Income/Income.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import { InnerLayout } from '../../styles/layouts';
 import { useGlobalContext } from '../../context/globalContext';
+import Form from '../Form/Form';
 
 function Income() {
   const {addIncome} = useGlobalContext()
@@ -10,7 +11,9 @@ function Income() {
       <InnerLayout>
         <h1>Income </h1>
         <div className='income-content'>
-          <div className='form-container'></div>
+          <div className='form-container'>
+            <Form />
+          </div>
           <div className='incomes'>
 
           </div>
