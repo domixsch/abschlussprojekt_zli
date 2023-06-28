755d3f92965b9199e2bb5fcb72856af638460976
Dominik Schweizer <dominikschweizer06@gmail.com>
2023-06-28 16:33:59 +0200

    .env changed

--- ausgaben-tracker/code/backend/controllers/income.js
+++ ausgaben-tracker/code/backend/controllers/income.js
@@ -12,7 +12,7 @@ exports.addIncome = async(req, res) =>  {
     })
 
 try {
-    //validation
+    //validierung
     if(!title ||!category ||!description ||!date){
         return res.status(400).json({message: 'All fields are required'})
     }
